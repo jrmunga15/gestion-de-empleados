04ad6136414a35f759cd06f0247f8aa57a7b6fa7
jrmunga15 <93112087+jrmunga15@users.noreply.github.com>
2023-11-22 22:35:06 +0100

    Add files via upload

---
+++ empleados_bd/delete.php
@@ -0,0 +1,32 @@
+<?php
+include 'database.php';
+include 'Employee.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $employeeId = $_POST['id'];
+
+    $employee = new Employee($employeeId, "", "", "", "", "");
+
+    if ($employee->delete()) {
+        echo "Empleado eliminado con éxito.";
+        // Botón para regresar a la lista de empleados
+        echo "<br><a href='index.php'><button>Regresar a la Lista de Empleados</button></a>";
+    } else {
+        echo "Error al eliminar el empleado.";
+        // Botón para regresar a la lista de empleados
+        echo "<br><a href='index.php'><button>Regresar a la Lista de Empleados</button></a>";
+    }
+} else {
+    if (isset($_GET['id'])) {
+        $employeeId = $_GET['id'];
+        echo "<h1>Confirmación de Eliminación</h1>";
+        echo "<p>¿Estás seguro de que deseas eliminar al empleado con ID: $employeeId?</p>";
+        echo "<form method='post' action=''>
+                <input type='hidden' name='id' value='$employeeId'>
+                <input type='submit' value='Eliminar'>
+              </form>";
+    } else {
+        echo "No se ha especificado un ID de empleado válido.";
+    }
+}
+?>
